c5275c87eace05762af5896437df923fba431b51
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-02-03 23:04:27 +0530

    Initial Commit

---
+++ Fairshare/FairshareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairshareApp.swift
+//  Fairshare
+//
+//  Created by Miyuru Gunarathna on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct FairshareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FairshareTests/FairshareTests.swift
@@ -0,0 +1,17 @@
+//
+//  FairshareTests.swift
+//  FairshareTests
+//
+//  Created by Miyuru Gunarathna on 2025-02-03.
+//
+
+import Testing
+@testable import Fairshare
+
+struct FairshareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
